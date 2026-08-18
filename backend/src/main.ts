71fcb9ee3c5384e5aea03017941cc359c40cfab5
JerwinIT <jerwinperiaIT@gmail.com>
2026-08-18 19:25:06 +0800

    created env.example for frontend and backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   });
   await app.listen(process.env.PORT ?? 5000);
 }
